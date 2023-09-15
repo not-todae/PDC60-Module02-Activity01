7bad31ee8f047374a339a3c4b6318d7355dc0824
not-todae <echon.davidaaron@auf.edu.ph>
2023-09-15 11:30:09 +0800

    first commit

---
+++ App1/Model/TaskModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1.Model
+{
+    class TaskModel
+    {
+        public string Subjectcode { get; set; }
+        public string SubjectTitle { get; set; }
+
+        public int Unit { get; set; }
+    }
+}
---
+++ App1/ViewModel/TaskViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using App1.Model;
+namespace App1.ViewModel
+{
+    class TaskViewModel
+    {
+        public TaskModel TaskModelSet { get; set; }
+        public TaskViewModel()
+        {
+            TaskModelSet = new TaskModel
+            {
+                Subjectcode = "PDC06",
+                SubjectTitle = "Xamarin Mobile Programming",
+                Unit = 3,
+            };
+        }
+
+    }
+}
